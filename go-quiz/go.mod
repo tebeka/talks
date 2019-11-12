dc9e1581622ffa10a2adba2ae267e29b0d8c6b5a
Miki Tebeka <miki.tebeka@gmail.com>
2019-11-12 14:58:58 +0200

    test (WIP)

--- go-quiz/go.mod
+++ go-quiz/go.mod
@@ -1,3 +1,5 @@
 module goquiz
 
 go 1.13
+
+require github.com/stretchr/testify v1.4.0
---
+++ go-quiz/slide_test.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	// *B:* panic
+	bulletRe = regexp.MustCompile(`\*[ABCD]:\* `)
+)
+
+type TestCase struct {
+	file    string
+	answers []string
+}
+
+func parseSlides(path string, t *testing.T) []*TestCase {
+	require := require.New(t)
+	file, err := os.Open(path)
+	require.NoError(err, "open slides")
+
+	s := bufio.NewScanner(file)
+	var cases []*TestCase
+	var tc *TestCase
+	lnum := 0
+	for s.Scan() {
+		lnum++
+		line := s.Text()
+		// .play -edit -numbers empty-map-ok.go
+		if strings.Contains(s.Text(), ".play") {
+			if tc != nil {
+				require.Equalf(4, len(tc.answers), "case before %d", lnum)
+				cases = append(cases, tc)
+			}
+			fields := strings.Fields(line)
+			tc = &TestCase{file: fields[len(fields)-1]}
+			continue
+		}
+
+		match := bulletRe.FindString(line)
+		if match == "" {
+			continue
+		}
+		text := line[len(match):]
+		require.Truef(tc != nil, "%d: bullet not in section", lnum)
+		tc.answers = append(tc.answers, text)
+	}
+	require.NoError(s.Err(), "scan")
+
+	if tc != nil {
+		require.Equalf(4, len(tc.answers), "last case")
+		cases = append(cases, tc)
+	}
+	return cases
+}
+
+func hasAnswer(output string, answers []string) bool {
+	for _, ans := range answers {
+		if strings.Contains(output, ans) {
+			return true
+		}
+	}
+	return false
+}
+
+func isDeadlock(answers []string) bool {
+	for _, ans := range answers {
+		if strings.Contains(ans, "Deadlock") {
+			return true
+		}
+	}
+	return false
+}
+
+func isFail(answers []string) bool {
+	for _, ans := range answers {
+		for _, text := range []string{"panic", "Won't compile"} {
+			if strings.Contains(ans, text) {
+				return true
+			}
+		}
+	}
+	return true
+}
+
+func TestSlides(t *testing.T) {
+	cases := parseSlides("quiz.slide", t)
+	for _, tc := range cases {
+		t.Run(tc.file, func(t *testing.T) {
+			require := require.New(t)
+			cmd := exec.Command("go", "run", tc.file)
+			data, err := cmd.CombinedOutput()
+			output := string(data)
+			if err == nil {
+				require.True(hasAnswer(output, tc.answers))
+			} else {
+				if isDeadlock(tc.answers) {
+					require.True(strings.Contains(output, "deadlock!"), "deadlock")
+				} else {
+					require.True(isFail(tc.answers), "fail")
+				}
+			}
+		})
+	}
+}
