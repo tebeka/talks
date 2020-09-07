0ce1862c6daf2d2cf353095ae7d563eb6e36da54
Miki Tebeka <miki.tebeka@gmail.com>
2020-09-07 21:20:36 +0300

    better example

--- pyweb-quiz/teasers/vote.py
+++ pyweb-quiz/teasers/vote.py
@@ -2,6 +2,6 @@
 
 import re
 
-text = 'The vote was 21 in favour, 13 against and 10 abstentions'
+text = 'The vote was 65 in favour, 43 against and 21 abstentions'
 match = re.search(r'(\d+).*(\d+).*(\d+)', text)
 print(match.group(1), match.group(2), match.group(3))
