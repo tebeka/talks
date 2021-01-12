5c8cca427c0710a3a34d4d77cb80cdb2f50a0a5a
Miki Tebeka <miki.tebeka@gmail.com>
2021-01-12 14:51:52 +0200

    weighted probs for birthday

--- pyweb-sim/birthday_freq.py
+++ pyweb-sim/birthday_freq.py
@@ -1,12 +1,35 @@
 # data from https://github.com/fivethirtyeight/data/tree/master/births
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
+from tqdm import tqdm
 
-df = pd.read_csv('US_births_2000-2014_SSA.csv')
-df.rename(columns={'date_of_month': 'day'}, inplace=True)
-df = df[df['year'] == 2013]
-df.index = pd.to_datetime(df[['year', 'month', 'day']])
-ax = df['births'].plot()
-ax.set_ylabel('Number of births')
-ax.set_title('US Births 2013')
-plt.savefig('birth_rate.png')
+
+def load_birthdays(csv_file):
+    """"Load probability of birthdays from csv_file. Return days,
+    probabilities."""
+    df = pd.read_csv(csv_file)
+    df.rename(columns={'date_of_month': 'day'}, inplace=True)
+    # df = df[df['year'] == 2013]
+    df.index = pd.to_datetime(df[['year', 'month', 'day']])
+    df['bday'] = df.index.strftime('%m-%d')
+    probs = df.groupby('bday')['births'].sum()
+    probs /= probs.sum()
+    return probs.index, probs.values
+
+
+def dup_in_group(size, days, probs):
+    """Return True if there's at least one duplicate birthday in a random
+    group of size.
+    """
+    group = np.random.choice(days, size, p=probs)
+    return len(set(group)) < len(group)
+
+
+days, probs = load_birthdays('US_births_2000-2014_SSA.csv')
+n, group_size, dups = 1_000_000, 23, 0
+for _ in tqdm(range(n)):
+    if dup_in_group(group_size, days, probs):
+        dups += 1
+
+frac = dups / n
+print(f'{frac:.2f}')
