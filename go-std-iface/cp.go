98d365a4a4ab0e5e0252a221cef768ae6a44cfcd
Miki Tebeka <miki@353solutions.com>
2022-01-22 09:06:13 +0200

    rename

---
+++ go-std-iface/cp.go
@@ -0,0 +1,9 @@
+package main
+
+// START_COPY OMIT
+// Copy copies from src to dst until either EOF is reached
+func Copy(dst Writer, src Reader) (written int64, err error) {
+	// Redacted
+}
+
+// END_COPY OMIT
---
+++ go-std-iface/ideas.md
@@ -0,0 +1,9 @@
+io.Reader/io.Writer
+    - sha1
+    - rot13
+    - io.Pipe
+
+json.Marshaler
+encoding.TextMarshaler
+fmt.Stringer
+fmt.Formatter
