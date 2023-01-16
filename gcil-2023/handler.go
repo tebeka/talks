17d2a59586969daea417a10ecaed21b724973eeb
Miki Tebeka <miki@353solutions.com>
2023-01-16 11:39:58 +0200

    play handler

--- gcil-2023/handler.go
+++ gcil-2023/handler.go
@@ -1,7 +1,12 @@
 package main
 
+import (
+	"fmt"
+)
+
 type Message struct {
-	// ...
+	User string
+	Body string
 }
 
 // AAA OMIT
@@ -16,14 +21,39 @@ type Provider interface {
 func handler(p Provider) {
 	for { // HL
 		msg := p.Next()
-		if msg == nil { // no more
+		if msg == nil { // HL
 			break
 		}
-		// TODO: Handle msg
+		fmt.Printf("%+v\n", msg)
 	}
 }
 
 // END OMIT
 
+type prov struct {
+	i     int
+	count int
+}
+
+func (p *prov) Next() *Message {
+	if p.i == p.count {
+		return nil
+	}
+	p.i++
+
+	msgs := []string{
+		":(){ :|:& };:",
+		"sudo rm -rf /",
+		"sudo shutdown -h now",
+	}
+
+	return &Message{
+		User: "eliot",
+		Body: msgs[p.i-1],
+	}
+}
+
 func main() {
+	p := prov{count: 3}
+	handler(&p)
 }
