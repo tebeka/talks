77ed785dd0da2f2bbc7956c1d785559016596106
Miki Tebeka <miki@353solutions.com>
2023-01-24 08:14:43 +0200

    simpler

--- gcil-2023/scores.go
+++ gcil-2023/scores.go
@@ -4,18 +4,20 @@ import (
 	"fmt"
 )
 
+type Player struct {
+	Name   string
+	Points int
+}
+
 // START OMIT
 func main() {
-	var players = []struct {
-		name   string
-		points int
-	}{
+	players := []Player{
 		{"Rick", 1_000_000},
 		{"Morty", 13},
 	}
 
 	for _, player := range players { // HL
-		player.points += 353
+		player.Points += 353
 	}
 	fmt.Printf("%v\n", players)
 	// [{Rick 1000000} {Morty 13}]
--- gcil-2023/scores_ref.go
+++ gcil-2023/scores_ref.go
@@ -4,18 +4,20 @@ import (
 	"fmt"
 )
 
+type Player struct {
+	Name   string
+	Points int
+}
+
 // START OMIT
 func main() {
-	var players = []struct {
-		name   string
-		points int
-	}{
+	players := []Player{
 		{"Rick", 1_000_000},
 		{"Morty", 13},
 	}
 
 	for i := range players { // HL
-		players[i].points += 353
+		players[i].Points += 353
 	}
 	fmt.Printf("%v\n", players)
 	// [{Rick 1000353} {Morty 366}]
