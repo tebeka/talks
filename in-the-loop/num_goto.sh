97d5fb081e78d50830523884e9ba2a2aedcdc08f
Miki Tebeka <miki@353solutions.com>
2024-06-20 16:11:27 +0200

    1.22

--- in-the-loop/num_goto.sh
+++ in-the-loop/num_goto.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-ag --vimgrep -s --go 'goto\s+' ~/sdk/go1.21.0/src | \
+ag --vimgrep -s --go 'goto\s+' ~/sdk/go1.22.0/src | \
     grep -v testdata  | \
     grep -v _test.go | \
     wc -l
